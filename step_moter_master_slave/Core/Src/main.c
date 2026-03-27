c27947193d80c2ae1ffa3dbd8ea1f62098111cc2
jinbaocheng <3504162327@qq.com>
2026-03-27 22:40:56 +0800

    feat: 完成主从定时器双线架构

---
+++ step_moter_master_slave/Core/Src/main.c
@@ -0,0 +1,287 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int target_steps = 3200;      // 总共要走的步数
+int current_step = 0;         // 已经走了的步数 (注意：昨天我们是倒着减，今天为了分段，我们正着加)
+
+int accel_steps = 800;        // 用多少步来加速
+int decel_steps = 800;        // 用多少步来减速
+
+uint16_t arr_current = 2000;  // 起步时的 ARR (很大，意味着起步很慢)
+uint16_t arr_min = 500;       // 巡航时的最小 ARR (速度最快)
+uint16_t arr_step = 2;        // 每次进入中断，ARR 改变的步长 (比如每次减2
+
+int is_motor_running = 0;  // 电机运行标志位：0代表停止，1代表正在运行
+
+
+
+
+void Profile_Check()
+{        int expected_accel = 800;
+         int expected_decel = 800;
+if(target_steps<( expected_accel+expected_decel))
+{
+    expected_accel=target_steps/2;
+    expected_decel=expected_accel;
+}
+else {
+    accel_steps= expected_accel;
+     decel_steps= expected_decel;
+}
+}
+void Moter_Start(void)
+   {
+    Profile_Check();
+ 
+    // 1. 设置从定时器 (TIM3) 的目标步数 (3200步)
+  
+    __HAL_TIM_SET_AUTORELOAD(&htim3, 3200 - 1); 
+
+    // 2. 清空 TIM3 的当前计数值，确保从 0 开始数
+    __HAL_TIM_SET_COUNTER(&htim3, 0);
+    
+    // 3.重置起步速度
+    arr_current = 2000;  
+       
+    // 4. 设置主定时器 (TIM4) 的初始速度和占空比 (起步速度)
+    __HAL_TIM_SET_AUTORELOAD(&htim4, 1999);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 1000);
+       
+      // 5.标志位点亮
+    is_motor_running = 1;
+       
+    //6. 开启中断
+    HAL_TIM_Base_Start_IT(&htim3);
+
+    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+ 
+    HAL_TIM_Base_Start_IT(&htim6);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_USART1_UART_Init();
+  MX_TIM6_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+      Moter_Start();
+      HAL_Delay(8000);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+// 当定时器的一个周期跑完（即 TIM3 数到了 ARR 的值）时，自动进入这里
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    // 确认是 TIM3（计步器）发来的停火信号
+    if (htim->Instance == TIM3)
+    {
+        // 停掉 TIM4 的 PWM
+        HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_1);
+        //停掉 TIM6 
+        HAL_TIM_Base_Stop_IT(&htim6);
+        // 停掉 TIM3 自己
+        HAL_TIM_Base_Stop_IT(&htim3);
+        
+        // 标志位熄灭
+        is_motor_running = 0; 
+    }
+    else if(htim->Instance == TIM6)
+    {    
+        // 标志位安检
+        if (is_motor_running == 0) return;
+        
+        current_step =__HAL_TIM_GET_COUNTER(&htim3);
+        if(current_step<=accel_steps)
+        {
+        
+          arr_current-=arr_step;
+        __HAL_TIM_SET_AUTORELOAD(&htim4,arr_current);
+        __HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_1,arr_current/2);
+        }
+        else if(current_step > (target_steps - decel_steps))
+        {
+            if (arr_current < 2000) // 不超过起步速度
+            {
+                arr_current += arr_step;
+            }
+            // 写入新的频率，并严格保持 50%占空比
+            __HAL_TIM_SET_AUTORELOAD(&htim4, arr_current);
+            __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, arr_current / 2);
+        }
+    }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
